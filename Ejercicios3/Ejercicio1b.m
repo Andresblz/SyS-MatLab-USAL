01ee5246beb4ae40e761e6a77b2467563099c22f
Andresblz <57406530+Andresblz@users.noreply.github.com>
2021-01-02 13:46:09 +0100

    Update Ejercicio1b.m

--- Ejercicios3/Ejercicio1b.m
+++ Ejercicios3/Ejercicio1b.m
@@ -1,4 +1,4 @@
-% SESIÓN 3 SEÑALES Y SISTEMAS
+% SESIÓN 3 - SEÑALES Y SISTEMAS
 % Ejercicio 1
 % Apartado b
 
@@ -18,9 +18,3 @@ hold on
 [w_esp,F] = espectro(t,y,5);
 plot(w_esp,F,'-b')
 hold off
-
-% La única diferencia que observamos en la gráfica al hacer zoom es que al
-% representar mediante stem() obtenemos valores discretos, en la función
-% plot() observamos un valor que parece continuo, pero se interpreta como 
-% un espectro discreto. Debajo de esta gráfica puede haber pequeñas subidas
-% y bajadas, así como picos, pero esto se puede obviar.
