51168f35eefd8c9347727f8f0505e3904fb712f5
Andresblz <57406530+Andresblz@users.noreply.github.com>
2021-01-02 13:43:40 +0100

    Update Ejercicio2c.m

--- Ejercicios2/Ejercicio2c.m
+++ Ejercicios2/Ejercicio2c.m
@@ -1,4 +1,4 @@
-% SESIÓN 2 SEÑALES Y SISTEMAS 
+% SESIÓN 2 - SEÑALES Y SISTEMAS 
 % Ejercicio 2
 % Apartado c
 
@@ -14,13 +14,3 @@
 
 % Mostramos los coeficientes obtenidos
 coef
-
-% 1. Valor de los 10 primeros armónicos: 
-%    2.5000    1.5915    0.0000   -0.5305   -0.0000    0.3183    0.0000   -0.2274   -0.0000    0.1768    0.0000
-%
-% 2. ¿Algún armónico se anula? ¿Por qué?
-%    Si, se anulan los armónicos c2, c5, c6, c8, c10
-%
-% 3. ¿Cuánto vale el coeficiente c0 y qué representa físicamente?
-%    El valor de c0 es 2.5000 y representa
-%
